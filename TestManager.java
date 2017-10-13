9786b1189f34c19234665515b07f8f68afe4bee9
dudo <dudo@zone07-wa.cs.ucalgary.ca>
2017-10-13 10:30:58 -0600

    Created and added junit test for Manager

---
+++ TestManager.java
@@ -0,0 +1,120 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestManager {
+
+	@Test
+	public void testSingleAdd() {
+		Manager aManager = new Manager();
+		aManager.add("title", "cast1", "cast2", "cast3", "genre", 5, false);
+		MovieNode movie1 = aManager.getHead();
+
+		assertEquals("title", movie1.getName());
+		assertEquals("cast1", movie1.getCast1());
+		assertEquals("cast2", movie1.getCast2());
+		assertEquals("cast3", movie1.getCast3());
+		assertEquals("genre", movie1.getGenre());
+		assertEquals(5, movie1.getRating());
+	}
+
+	@Test
+	public void testMultipleAdd() {
+		Manager aManager = new Manager();
+		aManager.add("title", "cast1", "cast2", "cast3", "genre", 5, false);
+		aManager.add("title2", "cast4", "cast5", "cast6", "genre2", 2, false);
+		MovieNode movie1 = aManager.getHead();
+		MovieNode movie2 = aManager.getHead().getNext();
+
+		assertEquals("title", movie1.getName());
+		assertEquals("cast1", movie1.getCast1());
+		assertEquals("cast2", movie1.getCast2());
+		assertEquals("cast3", movie1.getCast3());
+		assertEquals("genre", movie1.getGenre());
+		assertEquals(5, movie1.getRating());
+
+		assertEquals("title2", movie2.getName());
+		assertEquals("cast4", movie2.getCast1());
+		assertEquals("cast5", movie2.getCast2());
+		assertEquals("cast6", movie2.getCast3());
+		assertEquals("genre2", movie2.getGenre());
+		assertEquals(2, movie2.getRating());
+	}
+
+	@Test
+	public void testErase() {
+		Manager aManager = new Manager();
+		aManager.add("title", "cast1", "cast2", "cast3", "genre", 5, false);
+		aManager.add("title2", "cast4", "cast5", "cast6", "genre2", 2, false);
+		aManager.eraseList();
+
+		assertEquals(null, aManager.getHead());
+	}
+
+	@Test
+	public void testRemoveSingle() {
+		Manager aManager = new Manager();
+		aManager.add("title", "cast1", "cast2", "cast3", "genre", 5, false);
+		aManager.remove("title");
+
+		assertEquals(null, aManager.getHead());
+	}
+
+	@Test
+	public void testRemoveHeadForMultiple() {
+		Manager aManager = new Manager();
+		aManager.add("title", "cast1", "cast2", "cast3", "genre", 5, false);
+		aManager.add("title2", "cast4", "cast5", "cast6", "genre2", 2, false);
+		aManager.remove("title");
+		MovieNode movie2 = aManager.getHead();
+		assertEquals("title2", movie2.getName());
+		assertEquals("cast4", movie2.getCast1());
+		assertEquals("cast5", movie2.getCast2());
+		assertEquals("cast6", movie2.getCast3());
+		assertEquals("genre2", movie2.getGenre());
+		assertEquals(2, movie2.getRating());
+	}
+
+	@Test
+	public void testRemoveTailForMultiple() {
+		Manager aManager = new Manager();
+		aManager.add("title", "cast1", "cast2", "cast3", "genre", 5, false);
+		aManager.add("title2", "cast4", "cast5", "cast6", "genre2", 2, false);
+		aManager.remove("title2");
+		MovieNode movie1 = aManager.getHead();
+
+		assertEquals("title", movie1.getName());
+		assertEquals("cast1", movie1.getCast1());
+		assertEquals("cast2", movie1.getCast2());
+		assertEquals("cast3", movie1.getCast3());
+		assertEquals("genre", movie1.getGenre());
+		assertEquals(5, movie1.getRating());
+	}
+
+	@Test
+	public void testRemoveBodyForMultiple() {
+		Manager aManager = new Manager();
+		aManager.add("title", "cast1", "cast2", "cast3", "genre", 5, false);
+		aManager.add("title2", "cast4", "cast5", "cast6", "genre2", 2, false);
+		aManager.add("title3", "cast7", "cast8", "cast9", "genre3", 3, false);
+		aManager.remove("title2");
+		MovieNode movie1 = aManager.getHead();
+		MovieNode movie3 = aManager.getHead().getNext();
+
+		assertEquals("title", movie1.getName());
+		assertEquals("cast1", movie1.getCast1());
+		assertEquals("cast2", movie1.getCast2());
+		assertEquals("cast3", movie1.getCast3());
+		assertEquals("genre", movie1.getGenre());
+		assertEquals(5, movie1.getRating());
+
+		assertEquals("title3", movie3.getName());
+		assertEquals("cast7", movie3.getCast1());
+		assertEquals("cast8", movie3.getCast2());
+		assertEquals("cast9", movie3.getCast3());
+		assertEquals("genre3", movie3.getGenre());
+		assertEquals(3, movie3.getRating());
+	}
+
+
+}
